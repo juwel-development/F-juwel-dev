6e5dc1742ed9b1b010b90da6bd1ce35c8dd1f8e6
Jens-Uwe Lössl <mail@jens-uwe-loessl.de>
2018-11-03 20:07:50 +0100

    basic layout

--- src/vue/sections/Index.vue
+++ src/vue/sections/Index.vue
@@ -1,7 +1,14 @@
 <template>
     <section class="index-section">
-        <h1>I love creating apps that scale.</h1>
-        <p>{coming soon}</p>
+        <div class="left-column">
+            <h1>I love creating apps that scale.</h1>
+            <p>{coming soon}</p>
+        </div>
+        <div class="sidebar">
+            <div class="item bg-citrus"><a href="#why">Why</a></div>
+            <div class="item bg-apple"><a href="#how">How</a></div>
+            <div class="item bg-apricot"><a href="#technologie">Technology</a></div>
+        </div>
     </section>
 </template>
 
