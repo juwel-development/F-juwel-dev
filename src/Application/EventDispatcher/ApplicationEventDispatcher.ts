4b14bad3a28fe65f11e7c7168c6cd1831f97ddd1
Jens-Uwe Loessl <mail@jens-uwe-loessl.de>
2024-05-19 01:54:50 +0200

    chore: inject GlobalEventStream

--- src/Application/EventDispatcher/ApplicationEventDispatcher.ts
+++ src/Application/EventDispatcher/ApplicationEventDispatcher.ts
@@ -1,15 +1,20 @@
-import { GlobalEvent$ } from '@juwel-development/react-observable-tools';
+import { GlobalEvent$, GlobalEventStreamToken } from '@juwel-development/react-observable-tools';
 import { PageRedirectCommandEvent } from 'Domain/Application/Event/PageRedirectCommandEvent';
-import { injectable } from 'tsyringe';
+import { inject, injectable } from 'tsyringe';
 
 @injectable()
 export class ApplicationEventDispatcher {
+  public constructor(
+    @inject(GlobalEventStreamToken)
+    private readonly globalEvent$: typeof GlobalEvent$,
+  ) {}
+
   public async redirectCommand(to: string) {
     await import('Domain/Application/ApplicationModule');
 
     const event = new PageRedirectCommandEvent();
     event.payload.to = to;
 
-    GlobalEvent$.next(event);
+    this.globalEvent$.next(event);
   }
 }
