82b27498cb780abecd74722cf935da1406ec9dd1
Jens-Uwe Loessl <mail@jens-uwe-loessl.de>
2024-05-23 00:22:31 +0200

    feat: Improve seo

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,6 +3,7 @@ import { defineCollection, z } from 'astro:content';
 const postsCollection = defineCollection({
     schema: z.object({
         title: z.string(),
+        description: z.string(),
         publishedAt: z.date(),
         draft: z.boolean().optional(),
     }),
