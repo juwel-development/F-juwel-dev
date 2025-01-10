e04ef116722fb90e666910ae22b964db1441e413
Jens-Uwe Lössl <jens@nuwork.de>
2025-01-10 16:52:32 +0100

    feat: minimize our stack

--- src/components/js/Atom/Typography/Strong.tsx
+++ src/components/js/Atom/Typography/Strong.tsx
@@ -1,5 +1,5 @@
 import type { FunctionComponent, PropsWithChildren } from 'react';
 
 export const Strong: FunctionComponent<PropsWithChildren> = ({ children }) => {
-  return <strong className="font-bold">{children}</strong>;
+  return <strong className="font-extrabold">{children}</strong>;
 };
