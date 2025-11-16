30b8c4867301008975972962f031957a09501b47
Jens-Uwe Loessl <mail@jens-uwe-loessl.de>
2025-11-16 03:37:19 +0100

    feat(CV): update CV

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,6 +5,7 @@ const postsCollection = defineCollection({
     title: z.string(),
     description: z.string(),
     publishedAt: z.date(),
+    tags: z.array(z.string()),
     author: z.string(),
     draft: z.boolean().optional(),
   }),
